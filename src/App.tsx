766c977a4811cb059bef6f1ea2700d7b00e4279d
Royman <royman.dev@gmail.com>
2022-05-17 03:43:13 +0700

    feat: add starting page

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,44 @@
-import { useState } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { SiVite, SiReact, SiTypescript, SiTailwindcss } from 'react-icons/si';
+
+interface TechProps {
+  Icon: React.ElementType;
+  children: string;
+  href: string;
+}
+
+function Tech({ Icon, children, href }: TechProps) {
+  return (
+    <a
+      className="flex gap-3 items-center py-3 px-6 hover:text-white bg-gray-800 hover:bg-gray-700 rounded-xl shadow transition-colors"
+      href={href}
+      rel="noopener noreferrer"
+      target="_blank"
+    >
+      <Icon className="w-6 h-6" />
+      {children}
+    </a>
+  );
+}
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((prevCount) => prevCount + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+    <div className="flex flex-col gap-8 justify-center items-center h-screen text-center text-gray-300 bg-gray-900">
+      <p className="text-5xl ">Hello, This is</p>
+      <h1 className="text-8xl font-bold text-white">React TS Starter</h1>
+      <div className="flex flex-wrap gap-3 justify-center max-w-3xl text-2xl">
+        <Tech href="https://vitejs.dev/" Icon={SiVite}>
+          Vite
+        </Tech>
+        <Tech href="https://reactjs.org/" Icon={SiReact}>
+          React
+        </Tech>
+        <Tech href="https://www.typescriptlang.org/" Icon={SiTypescript}>
+          Typescript
+        </Tech>
+        <Tech href="https://tailwindcss.com/" Icon={SiTailwindcss}>
+          Tailwindcss
+        </Tech>
+      </div>
     </div>
   );
 }
