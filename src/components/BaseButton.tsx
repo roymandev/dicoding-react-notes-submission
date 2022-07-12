3a3964d7b30ef5babe9838e205deee0fa20ac779
Royman <royman.dev@gmail.com>
2022-07-13 01:32:04 +0700

    feat: add BaseButton component

---
+++ src/components/BaseButton.tsx
@@ -0,0 +1,22 @@
+import { twMerge } from 'tailwind-merge';
+
+interface BaseButtonProps extends React.ComponentPropsWithoutRef<'button'> {
+  variant?: keyof typeof variantClass;
+}
+
+const variantClass = {
+  default: 'hover:bg-slate-700 hover:shadow focus:bg-slate-700 focus:shadow',
+  primary: 'bg-slate-700 hover:bg-slate-600 shadow',
+};
+
+function BaseButton({ className, variant = 'default', ...rest }: BaseButtonProps) {
+  return (
+    <button
+      type="button"
+      {...rest}
+      className={twMerge('rounded text-slate-400 transition-colors outline-none', variantClass[variant], className)}
+    />
+  );
+}
+
+export default BaseButton;
