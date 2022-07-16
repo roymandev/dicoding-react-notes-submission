f6879dd2a422a916e470bdc5be551383c8691d60
Royman <royman.dev@gmail.com>
2022-07-17 01:59:18 +0700

    feat: NoteListItems add no items condition

--- src/components/NoteListItems.tsx
+++ src/components/NoteListItems.tsx
@@ -13,19 +13,23 @@ function NoteListItems({ notes }: NoteListItemsProps) {
 
   return (
     <ul className="flex flex-col gap-1 p-1 text-slate-400">
-      {notes.map((note) => (
-        <li key={note.id}>
-          <BaseButton
-            className={twMerge(
-              'py-1 px-2 w-full text-left',
-              selectedNote?.id === note.id && 'bg-slate-700 text-slate-300',
-            )}
-            onClick={() => setSelectedNote(note)}
-          >
-            {note.title || 'Untitled'}
-          </BaseButton>
-        </li>
-      ))}
+      {notes.length ? (
+        notes.map((note) => (
+          <li key={note.id}>
+            <BaseButton
+              className={twMerge(
+                'py-1 px-2 w-full text-left',
+                selectedNote?.id === note.id && 'bg-slate-700 text-slate-300',
+              )}
+              onClick={() => setSelectedNote(note)}
+            >
+              {note.title || 'Untitled'}
+            </BaseButton>
+          </li>
+        ))
+      ) : (
+        <li className="px-1">No notes</li>
+      )}
     </ul>
   );
 }
